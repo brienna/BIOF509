6e112fee526e37447595ade68966cec419556c74
Brienna Herold <brienna.herold@gmail.com>
2016-02-25 16:30:22 -0500

    Solved Wk03 exercise

--- Wk03/cookbook.py
+++ Wk03/cookbook.py
@@ -1,110 +1,109 @@
 
-# Description: The cookbook shows options from which you choose a category of recipes. 
-# Inside the category of recipes, you see a list of recipes to choose from. Once you choose
-# a recipe, you view its ingredients, equipment needed and instructions. The recipe is 
-# scalable to different numbers of servings with the amount of ingredients adjusted
-# appropriately and viewable in metric and imperial units. Nutritional information should 
-# be tracked.
+class Cookbook(object): 
+	"""A class definition for a cookbook. The following attributes are supported:
+    
+    Attributes:
+    name: A string representing the Cookbook's name
+    recipes: A list of recipe objects"""
+
+	def __init__(self, name):
+		"""Initalize a Cookbook object with name set to the value supplied, and initialize recipes array"""
+		self.name = name
+		self.recipes = []
+
+	def open(self, recipe_name):
+		"""Return a recipe. 
+
+		Arguments received:
+		recipe_name -- string representing the recipe name
+		"""
+		for i in range(len(self.recipes)):
+			if self.recipes[i].name == recipe_name:
+				return self.recipes[i]
+
+	def add(self, recipe):
+		'''Append recipe object to Cookbook instance's recipes array
+
+		Arguments received:
+		recipe -- the recipe object to append
+		'''
+		self.recipes.append(recipe)
 
 
-def Cookbook(object): 
+class Recipe():
+	"""A class definition for a recipe. The following attributes are supported: 
 
-    def __init__:
-        pass
+	Attributes: 
+	name: a string representing the recipe's name
+	ingredients: a dictionary of ingredients the recipe needs 
+	equipment: a list of equipment the recipe needs
+	instructions: a string representing the instructions 
+	servings: an integer representing the number of people the recipe can serve
+	nutrition_info: a dictionary of nutrition info for each ingredient
+	cooking_time: a float representing cooking time in minutes
+	"""
 
-    def open():
-        '''Opens cookbook. 
+	def __init__(self, name, ingredients, equipment, instructions, servings, cooking_time):
+		"""Initialize a Recipe object with name, ingredients, equipment, instructions,
+        servings, nutrition_info, prep_time, cooking_time set to the values supplied"""
+		self.name = name
+		self.ingredients = ingredients  
+		self.equipment = equipment
+		self.instructions = instructions
+		self.servings = servings
+		# self.nutrition_info, loops thru ingredients dict to retrieve nutrition info from each ingredient
+		self.cooking_time = cooking_time
 
-        Takes and returns no arguments.
-        '''
-
-
-def Recipe():
-
-
-def Dessert(Recipe):
-
-def MainCourse(Recipe):
-
-def Appetizer(Recipe):
-
-def Ingredients(object):
-
-    def __init__(self, quantity):
-        self.quantity = quantity
-
-
-
-
-
-a_cookbook = Cookbook()  # instantiate Cookbook object
-a_cookbook.open()  # call a method on Cookbook instance
+	def __str__(self):
+		"""Return a basic string representation of the Recipe instance"""
+		return "Recipe: {0}.\nServes {1}.\nIngredients: {2}.\nInstructions: {3}.\nCooking time: {4} min.".format(self.name, self.servings, self.ingredients, self.instructions, self.cooking_time)
 
 
+class Ingredient(object):
+	'''A class definition for an ingredient. The following attributes are supported:
+
+	Attributes: 
+	name: a string representing the ingredient's name
+	quantity: a string representing the quantity of ingredient needed
+	nutrition_info: a string representing nutrition info of ingredient
+	'''
+
+	def __init__(self, name, quantity, nutrition_info):
+		'''Initialize a Ingredient object with name, quantity, nutrition_info set to values supplied'''
+		self.name = name
+		self.quantity = quantity
+		self.nutrition_info = nutrition_info
+
+	def __str__(self):
+		'''Return a basic string representation of the Ingredient instance'''
+		pass
+
+	def scale(self, old_servings, new_servings):
+		'''Scales quantity of ingredient according to new/old serving sizes
+
+		Arguments accepted:
+		old_servings -- an integer representing the number of people the recipe can currently serve
+		new_servings -- 'an integer representing the number of people the recipe needs to serve'''
+		pass
+
+	def convert(self):
+		'''Converts quantity of ingredient from metric to imperial, or vice versa'''
+		pass
 
 
-# DISCUSSED IN CLASS
+# create banana bread recipe
+flour = Ingredient('flour', '2 cups', 'flour nutrition info')
+baking_soda = Ingredient('baking soda', '1 tsp', 'baking soda nutrition info')
+salt = Ingredient('salt', '1/4 tsp', 'salt nutrition info')
+butter = Ingredient('butter', '1/2 cup', 'butter nutrition info')
+brown_sugar = Ingredient('brown sugar', '3/4 cup', 'brown sugar nutrition info')
+bananas = Ingredient('bananas', '2 1/3 cups', 'banana nutrition info')
+ingredients = [flour, baking_soda, salt, butter, brown_sugar, bananas]
+equipment = ['9x5 inch loaf pan', 'oven', 'bowl', 'wooden spoon', 'measuring cups/spoons']
+bananaBread = Recipe('banana bread', ingredients, equipment, 'instructions', 12, 80.0)
 
-# classes:
-# appetizers
-# main courses
-# desserts
-# recipe
-#      __Attributes/Methods__
-#             - preparation time
-#             - cooking time
-#             - serving portion
-#             - servings
-#             - name
-#             - nutritional information
-#             - ingredients
-#             - methods
-#             - scaling
-#             - equipment
-#             - special occasion 
-#             - instructions
-#      __Interactions__
-#             - Cookbook
-#             - Ingredients
-#             - Instructions
-#             - Nutrition
-#             - Equipment
-#             - Scaling
-#             - Portions
-# servings
-# ingredients
-#  - quantity (mass/volume)
-#     - units
-#  - nutritional info
-#  - specific dietary info
-#  - cost
-#  - storage temperature
-#  - sustitute options
-#  - name
-#  - translations
-#  - alternative names
-#  - special handling instructions
-#  - sources
-#      * can interact with the following classes:
-#      * recipe
-#      * Instructions
-#      * Equipment
-#      * nutrition 
-#      * scaling
-#      * portions 
-#      * cookbook
-#      * units
-# solid ingredients
-# liquid ingredients
-# equipment
-# instructions
-# nutritional info
-# style
-# units conversion
-# scaling 
-# portions
-# cookbook
+a_cookbook = Cookbook('My Delicious Noms')  # instantiate Cookbook
+a_cookbook.add(bananaBread)  # add banana bread recipe to the Cookbook instance
+recipe = a_cookbook.open('banana bread')  # open the banana bread recipe
+print(recipe)
 
-# (recipe class can have an attribute, list of ingredients which are instances of another class themselves)
-# (classes interact with each other often in an app like this)
-# (instructions can be a class, and an instance of it can be saved as an attribute in Recipe class)
