df8de4c02ef3d516c99b100f389e2c3d9006122f
Brienna Herold <brienna.herold@gmail.com>
2016-03-16 13:32:39 -0400

    Add support for both a first and last name

--- Wk06/06-storing-input/site.py
+++ Wk06/06-storing-input/site.py
@@ -12,7 +12,13 @@ from wtforms.validators import DataRequired
 
 
 class MyForm(Form):
-    name = StringField('name', validators=[DataRequired()])
+    first_name = StringField('First name:', validators=[DataRequired()])
+    last_name = StringField('Last name:', validators=[DataRequired()])
+    # Fields are responsible for rendering and data conversion
+    # They delegate to optional validators for data validation
+    # StringField is the base for most of the more complicated fields, 
+    # and it represents <input type="text">
+    # DataRequired checks that the data attribute on the field is a ‘true’ value
     
 
 from flask_sqlalchemy import SQLAlchemy
@@ -22,16 +28,19 @@ db = SQLAlchemy(app)
 
 
 class Visitor(db.Model):
+    # create columns for database table (not yet set)
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
 
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
     
 
     def __repr__(self):
-        return '<Visitor %r>' % self.name
+        return '<Visitor {0}, {1}>'.format(self.first_name, self.last_name)
     
     
 db.create_all()
@@ -40,13 +49,16 @@ db.create_all()
 def index():
     form = MyForm()
     previous_visitors = Visitor.query.order_by('id DESC').all()
+    # check if form submission is a valid POST request
     if form.validate_on_submit():
-        name = form.name.data
-        db.session.add(Visitor(name))
-        db.session.commit()
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        db.session.add(Visitor(first_name, last_name))  # set columns in database table
+        db.session.commit()  # write changes to the database
     else:
-        name = 'stranger'
-    return render_template('index.html', form=form, name=name, previous_visitors=previous_visitors)
+        first_name = 'stranger'
+        last_name = None
+    return render_template('index.html', form=form, first_name=first_name, last_name=last_name, previous_visitors=previous_visitors)
     
 @app.route('/custom/<custom>/')
 def custom(custom):
