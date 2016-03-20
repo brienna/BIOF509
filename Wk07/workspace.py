bc2ba3c3c0c37a98e645ce602478a9ac2f60a468
Brienna Herold <brienna.herold@gmail.com>
2016-03-20 01:32:20 -0400

    Add global title

--- Wk07/workspace.py
+++ Wk07/workspace.py
@@ -7,6 +7,8 @@ model = Model(RCT, {'n_arms': 2, 'epsilon':0.05}, weights=[0.1, 0.2])
 model.repeat_simulation()
 
 fig, axes = plt.subplots(1,3, figsize=(18,6))
+plt.suptitle("Performance of RCT and EpsilonGreedy algorithms", fontsize=20)
+
 model.plot_arm_frequency(ax=axes[0])
 model.plot_reward(ax=axes[1])
 model.plot_cumulative_reward(ax=axes[2])
@@ -24,10 +26,11 @@ plt.show()
 
 
 # plot different Epsilon values
-fig, axes = plt.subplots(1, 3, figsize=(18,6)) 
+fig, axes = plt.subplots(1, 3, figsize=(18,6))
+plt.suptitle("Performance of EpsilonGreedy algorithm with different epsilon values", fontsize=20)
+
 colors = ['red', 'orange', 'yellow', 'blue', 'purple', 'black']
 epsilon_values = [0.10, 1.0, 5.0, -.10, 3, 0.01]
-
 for color, value in zip(colors, epsilon_values):
 	model = Model(EpsilonGreedy, {'n_arms': 2, 'epsilon':value}, weights=[0.1, 0.2])
 	model.repeat_simulation()
@@ -37,4 +40,4 @@ for color, value in zip(colors, epsilon_values):
 
 plt.show()
 
-
+# ADD LEGEND TO FIGURE
