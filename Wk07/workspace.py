6a7b0fbcd86d7f55459544c5b0e2e18979acdec0
Brienna Herold <brienna.herold@gmail.com>
2016-03-20 00:26:12 -0400

    Plot the performance for different epsilon values

--- Wk07/workspace.py
+++ Wk07/workspace.py
@@ -12,9 +12,7 @@ model.plot_reward(ax=axes[1])
 model.plot_cumulative_reward(ax=axes[2])
 
 
-
-
-# plot EpsilonGreedy algorithm
+# plot EpsilonGreedy algorithm on same plots as RCT algorithm
 model = Model(EpsilonGreedy, {'n_arms': 2, 'epsilon':0.05}, weights=[0.1, 0.2])
 model.repeat_simulation()
 
@@ -23,3 +21,20 @@ model.plot_reward(ax=axes[1], colour='red')
 model.plot_cumulative_reward(ax=axes[2], colour='red')
 
 plt.show()
+
+
+# plot different Epsilon values
+fig, axes = plt.subplots(1, 3, figsize=(18,6)) 
+colors = ['red', 'orange', 'yellow', 'blue', 'purple', 'black']
+epsilon_values = [0.10, 1.0, 5.0, -.10, 3, 0.01]
+
+for color, value in zip(colors, epsilon_values):
+	model = Model(EpsilonGreedy, {'n_arms': 2, 'epsilon':value}, weights=[0.1, 0.2])
+	model.repeat_simulation()
+	model.plot_arm_frequency(ax=axes[0], colour=color)
+	model.plot_reward(ax=axes[1], colour=color)
+	model.plot_cumulative_reward(ax=axes[2], colour=color)
+
+plt.show()
+
+
